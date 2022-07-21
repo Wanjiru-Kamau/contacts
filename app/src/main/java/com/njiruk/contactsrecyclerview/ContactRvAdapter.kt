e46438e449be27101e495aac088ec4109cf31893
Wanjiru-Kamau <njirukmaureen@gmail.com>
2022-07-21 19:07:08 +0300

    pictures

--- app/src/main/java/com/njiruk/contactsrecyclerview/ContactRvAdapter.kt
+++ app/src/main/java/com/njiruk/contactsrecyclerview/ContactRvAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
 import com.njiruk.contactsrecyclerview.databinding.ContactListBinding
+import com.squareup.picasso.Picasso
 
 class ContactRvAdapter(var contact_list: List<Contacts>) :
     RecyclerView.Adapter<ContactViewHolder>() {
@@ -24,6 +25,13 @@ class ContactRvAdapter(var contact_list: List<Contacts>) :
         holder.binding.tvnumber.text = currentContact.phoneNumber
         holder.binding.tvemail.text = currentContact.email
         holder.binding.tvaddress.text = currentContact.address
+        Picasso.get()
+            .load(currentContact.image)
+            .resize(300, 300)
+            .centerCrop()
+            .placeholder(R.drawable.ic_baseline_person_24)
+            .into(holder.binding.imgName)
+
 
     }
 
--- app/src/main/java/com/njiruk/contactsrecyclerview/MainActivity.kt
+++ app/src/main/java/com/njiruk/contactsrecyclerview/MainActivity.kt
@@ -15,10 +15,10 @@ class MainActivity : AppCompatActivity() {
         displayContacts()
     }
     fun displayContacts(){
-        var contacts=Contacts("sakinah","saki@gmail.com","0724266263","nairobi","")
-        var contacts1=Contacts("Renee","renee@gmail.com","0724245333","ruai","")
-        var contacts2=Contacts("juma","juma@gmail.com","0724873767263","langata","")
-        var contacts3=Contacts("mellon","mellon@gmail.com","0735266263","dago","")
+        var contacts=Contacts("sakinah","saki@gmail.com","0724266263","nairobi","https://images.unsplash.com/photo-1583994009785-37ec30bf9342?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJsYWNrJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60")
+        var contacts1=Contacts("Renee","renee@gmail.com","0724245333","ruai","https://images.unsplash.com/photo-1589156280159-27698a70f29e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmxhY2slMjBwZW9wbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60")
+        var contacts2=Contacts("juma","juma@gmail.com","0724873767263","langata","https://images.unsplash.com/photo-1507152832244-10d45c7eda57?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGJsYWNrJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60")
+        var contacts3=Contacts("mellon","mellon@gmail.com","0735266263","dago","https://media.istockphoto.com/photos/headshot-of-mature-black-woman-walking-on-city-street-picture-id1364683713?b=1&k=20&m=1364683713&s=170667a&w=0&h=rHN6wZG4STOTU3BpbcHyPDELoHFuTS63hbnWt47f7zc=")
         var contactlist= listOf(contacts,contacts1,contacts2,contacts3)
 
         var contactAdapter=ContactRvAdapter(contactlist)
